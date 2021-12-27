241400ad34a33a5bb617b4f3e7ec8af4ef693dc1
blue_wizzrobe <kevin@mangakissa.nl>
2021-12-27 13:53:05 +0100

    opzet

---
+++ Voeding/src/main/java/nl/voeding/backend/Voeding/VoedingApplication.java
@@ -0,0 +1,13 @@
+package nl.voeding.backend.Voeding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoedingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoedingApplication.class, args);
+	}
+
+}
