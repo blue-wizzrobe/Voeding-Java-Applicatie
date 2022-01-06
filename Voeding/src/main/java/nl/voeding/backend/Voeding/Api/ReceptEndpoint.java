80097f5faf83bec90a70ce7079a4ff50ffa504b1
blue_wizzrobe <kevin@mangakissa.nl>
2022-01-06 15:56:48 +0100

    endpoint aangepast

--- Voeding/src/main/java/nl/voeding/backend/Voeding/Api/ReceptEndpoint.java
+++ Voeding/src/main/java/nl/voeding/backend/Voeding/Api/ReceptEndpoint.java
@@ -1,13 +1,11 @@
 package nl.voeding.backend.Voeding.Api;
 
+import nl.voeding.backend.Voeding.Controller.ReceptService;
+import nl.voeding.backend.Voeding.Domein.Recept;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import nl.voeding.backend.Voeding.Controller.ReceptService;
-import nl.voeding.backend.Voeding.Domein.Ingredient;
-import nl.voeding.backend.Voeding.Domein.Recept;
 
 @RestController
 public class ReceptEndpoint {
